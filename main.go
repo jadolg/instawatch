b97adf3a8b0d9b56f38a29452ae7bc05400eaa8a
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2026-02-25 21:45:18 +0100

    add video deletion scheduling after upload

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 )
 
 //go:embed all:static
@@ -27,6 +28,20 @@ var (
 	cache   = make(map[string]string)
 	cacheMu sync.RWMutex
 )
+
+func scheduleVideoDeletion(urlHash, videoPath string, delay time.Duration) {
+	time.AfterFunc(delay, func() {
+		cacheMu.Lock()
+		delete(cache, urlHash)
+		cacheMu.Unlock()
+
+		if err := os.Remove(videoPath); err != nil {
+			log.Printf("Failed to delete video %s: %v", videoPath, err)
+		} else {
+			log.Printf("Deleted video after %v: %s", delay, videoPath)
+		}
+	})
+}
 
 func init() {
 	templates = template.Must(template.ParseFS(templateFiles, "templates/*.html"))
@@ -110,6 +125,8 @@ func handleRoot(w http.ResponseWriter, r *http.Request, tmpDir string) {
 	cacheMu.Lock()
 	cache[urlHash] = videoPath
 	cacheMu.Unlock()
+
+	scheduleVideoDeletion(urlHash, videoPath, 1*time.Minute)
 
 	servePlayer(w, urlHash)
 }
