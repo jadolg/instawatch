cfe8312e0f68b067b3b44d78702559990e305ed0
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2026-03-09 23:52:29 +0100

    Add GitHub fork ribbon to the landing page and update CSP to allow its stylesheet.

--- middleware.go
+++ middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func securityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy",
-			"default-src 'self'; media-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; script-src 'self'")
+			"default-src 'self'; media-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com; font-src https://fonts.gstatic.com; script-src 'self'")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("Referrer-Policy", "no-referrer")
